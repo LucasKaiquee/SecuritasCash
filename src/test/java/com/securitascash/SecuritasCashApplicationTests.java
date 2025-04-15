1620bb5e8acc3b77da6848e37639089705adef6b
Luiz-Gomess <Lf17353@gmail.com>
2025-04-15 09:28:19 -0300

    feat: create folder structure

---
+++ src/test/java/com/securitascash/SecuritasCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitascash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritasCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
