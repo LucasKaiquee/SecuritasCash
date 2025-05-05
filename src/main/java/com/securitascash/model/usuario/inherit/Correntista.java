c64459d59296305800a3a149b2dd60fd3352bece
Luiz-Gomess <Lf17353@gmail.com>
2025-05-05 09:58:16 -0300

    feat: add attribute isBlocked in model.usuario.inherit.Correntista

--- src/main/java/com/securitascash/model/usuario/inherit/Correntista.java
+++ src/main/java/com/securitascash/model/usuario/inherit/Correntista.java
@@ -4,15 +4,17 @@ import com.securitascash.model.usuario.Usuario;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 @Entity
 @DiscriminatorValue("CORRENTISTA")
 public class Correntista extends Usuario{
-    
+    private boolean isBlocked;
 }
