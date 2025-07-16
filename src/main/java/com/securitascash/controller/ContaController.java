72836b833239d11c7d0d9978fe0e9914c08af174
LucasKaiquee <lucaskaique743@gmail.com>
2025-07-16 10:45:53 -0300

    feat: edit conta controller

--- src/main/java/com/securitascash/controller/ContaController.java
+++ src/main/java/com/securitascash/controller/ContaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.securitascash.dto.conta.ContaForm;
@@ -17,6 +18,10 @@ import com.securitascash.service.conta.ContaService;
 import com.securitascash.utils.Utils;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 @RequestMapping("/contas")
@@ -35,7 +40,7 @@ public class ContaController {
     
         List<Conta> contas = contaService.listarContasByUser(usuarioId);
         model.addAttribute("contas", contas);
-        return "contas";
+        return "contas/list";
     }
 
     @GetMapping("/criar")
@@ -45,7 +50,7 @@ public class ContaController {
         model.addAttribute("contaForm", new ContaForm());
         model.addAttribute("usuarioId", usuarioId);
 
-        return "criar-conta";
+        return "contas/form";
     }
 
     @PostMapping("/criar")
@@ -53,6 +58,34 @@ public class ContaController {
         contaService.criarConta(contaForm);
         redirectAttributes.addFlashAttribute("mensagem", "Conta criada com sucesso!");
         return "redirect:/contas";
+    }
+
+    @GetMapping("/{id}/editar")
+    public ModelAndView exibirFormularioEdicao(@PathVariable Long id, ModelAndView mav) {
+        Conta conta = contaService.buscarPorId(id);
+        if (conta == null) {
+            mav.setViewName("redirect:/contas/list");
+            return mav;
+        }
+        ContaForm contaForm = new ContaForm();
+
+        contaForm.setNumero(conta.getNumero());
+        contaForm.setDescricao(conta.getDescricao());
+        contaForm.setTipo(conta.getTipo());
+        contaForm.setUsuarioId(conta.getUsuario().getId());
+
+        mav.addObject("contaForm", contaForm);
+        
+        mav.setViewName("contas/form");
+        
+        return mav;
+    }
+
+    @PutMapping("editar/{id}")
+    public ModelAndView alterarConta(@PathVariable String id, ModelAndView mav) {
+        
+        
+        return mav;
     }
     
 }
