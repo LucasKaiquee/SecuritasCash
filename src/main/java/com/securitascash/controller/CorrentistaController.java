cb2b4f3599e600b9e9a461b79435c95bd731ab75
= <=>
2025-07-30 10:50:55 -0300

    feat: validation on Correntista form

--- src/main/java/com/securitascash/controller/CorrentistaController.java
+++ src/main/java/com/securitascash/controller/CorrentistaController.java
@@ -2,6 +2,7 @@ package com.securitascash.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.securitascash.model.usuario.inherit.Correntista;
 import com.securitascash.service.correntista.CorrentistaService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/correntistas")
@@ -34,10 +37,18 @@ public class CorrentistaController {
     }
 
     @PostMapping("/novo")
-    public String criarCorrentista(@ModelAttribute Correntista correntista, RedirectAttributes attr) {
+    public ModelAndView criarCorrentista(@Valid @ModelAttribute Correntista correntista, BindingResult result, RedirectAttributes attr, ModelAndView mav) {
+
+        if ( result.hasErrors() ) {
+            mav.setViewName("correntistas/form");
+            return mav;
+        }
+        
         correntistaService.criarCorrentista(correntista);
         attr.addFlashAttribute("mensagem", "Correntista criado com sucesso!");
-        return "redirect:/correntistas";
+
+        mav.setViewName("redirect:/correntistas");
+        return mav;
     }
     
 }
--- src/main/java/com/securitascash/model/usuario/Usuario.java
+++ src/main/java/com/securitascash/model/usuario/Usuario.java
@@ -13,6 +13,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,8 +30,14 @@ public abstract class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message= "Nome é obrigatório.")
     private String nome;
+
+    @NotNull(message= "Senha é obrigatório.")
     private String senha;
+
+    @NotBlank(message= "Email é obrigatório.")
     private String email;
 
     @OneToMany(mappedBy = "usuario", cascade = {CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST}, orphanRemoval = true)
