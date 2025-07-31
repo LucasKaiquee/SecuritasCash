e740dd853529a50e89bb697c9d8c579709b222dd
Luiz-Gomess <lf17353@gmail.com>
2025-07-31 09:31:33 -0300

    feat: list and creation of Categorias

---
+++ src/main/java/com/securitascash/controller/CategoriaController.java
@@ -0,0 +1,43 @@
+package com.securitascash.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.securitascash.dto.categoria.CategoriaForm;
+import com.securitascash.enums.Natureza;
+import com.securitascash.service.categoria.CategoriaService;
+
+@Controller
+@RequestMapping("/categorias")
+public class CategoriaController {
+    
+    @Autowired
+    CategoriaService service;
+
+    @GetMapping
+    public ModelAndView list (ModelAndView mav) {
+        mav.setViewName("categorias/list");
+        mav.addObject("categorias", service.listarCategorias());
+        return mav;
+    }
+
+    @GetMapping("/criar")
+    public ModelAndView form(ModelAndView mav) {
+        mav.setViewName("categorias/form");
+        mav.addObject("naturezas", Natureza.values());
+        mav.addObject("categoriaForm", new CategoriaForm());
+        return mav;
+    }
+
+    @PostMapping
+    public ModelAndView create(@ModelAttribute CategoriaForm categoriaForm, ModelAndView mav) {
+        service.criarCategoria(categoriaForm);
+        mav.setViewName("redirect:/categorias");
+        return mav;
+    }
+}
