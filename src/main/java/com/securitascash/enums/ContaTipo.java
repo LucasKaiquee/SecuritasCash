80d1d199b982c6beaabb2458ea0e407574f5597f
Luiz-Gomess <lf17353@gmail.com>
2025-08-08 09:35:14 -0300

    style: fields brazillian formatting

--- src/main/java/com/securitascash/enums/ContaTipo.java
+++ src/main/java/com/securitascash/enums/ContaTipo.java
@@ -1,6 +1,16 @@
 package com.securitascash.enums;
 
 public enum ContaTipo {
-    CORRENTE,
-    CARTAO_CREDITO;
+    CORRENTE("Conta Corrente"),
+    CARTAO_CREDITO("Cartão de Crédito");
+
+    private final String displayValue;
+
+    private ContaTipo(String displayValue) {
+        this.displayValue = displayValue;
+    }
+    
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
